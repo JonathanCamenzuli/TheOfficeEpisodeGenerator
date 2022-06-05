58088b4c61989ac513ea7b56f061d4266eeacda4
Jonathan Camenzuli <98608900+JonathanCamenzuli@users.noreply.github.com>
2022-06-06 01:26:47 +0200

    Added pathClass class

--- main.py
+++ main.py
@@ -20,15 +20,15 @@ class asciiArt():
     # Convert "images/logo.png" into ASCII art
     def getIntroLogo(self):
         # Pass the image as command line argument
-        image_path = "images/logo.png"
-        img = Image.open(image_path)
+        imagePath = "images/logo.png"
+        img = Image.open(imagePath)
 
         # Resize the image
         width, height = img.size
         aspect_ratio = height / width
-        new_width = 110 # orig: 120
-        new_height = aspect_ratio * new_width * 0.55
-        img = img.resize((new_width, int(new_height)))
+        newWidth = 110 # orig: 120
+        newHeight = aspect_ratio * newWidth * 0.55
+        img = img.resize((newWidth, int(newHeight)))
 
         # Convert image to greyscale format
         img = img.convert('L')
@@ -37,19 +37,19 @@ class asciiArt():
 
         # Replace each pixel with a character from array
         chars = ["@", "O", "#", "$", "%", "&", "*", "!", "~", "·", " "]
-        new_pixels = [chars[pixel//25] for pixel in pixels]
-        new_pixels = ''.join(new_pixels)
+        newPixels = [chars[pixel//25] for pixel in pixels]
+        newPixels = ''.join(newPixels)
 
         # Split string of chars into multiple strings of length equal to new width and create a list
-        new_pixels_count = len(new_pixels)
-        ascii_image = [new_pixels[index:index + new_width] for index in range(0, new_pixels_count, new_width)]
-        ascii_image = "\n".join(ascii_image)
-        print(ascii_image)
+        newPixelsCount = len(newPixels)
+        asciiImage = [newPixels[index:index + newWidth] for index in range(0, newPixelsCount, newWidth)]
+        asciiImage = "\n".join(asciiImage)
+        print(asciiImage)
 
     # Print "Episode Generator" string
     def getTitle(self):
-        bottom_text = pyfiglet.figlet_format("Episode Generator", font = "bubble" )
-        print(bottom_text)
+        bottomText = pyfiglet.figlet_format("Episode Generator", font = "bubble" )
+        print(bottomText)
 
     # Function that plays loading animation while
     # loading episode list
@@ -65,21 +65,21 @@ class episodeList():
 
     # Function that loads episode list
     def getEpList(self):
-        imdb_instance = imdb.IMDb()
-        imdb_code = "0386676"
+        imdbInstance = imdb.IMDb()
+        imdbCode = "0386676"
 
-        series = imdb_instance.get_movie(imdb_code)
-        imdb_instance.update(series, 'episodes')
+        series = imdbInstance.get_movie(imdbCode)
+        imdbInstance.update(series, 'episodes')
         episodes = series.data['episodes']
         with open("episode_list.txt", "w") as f:
             for i in episodes.keys():
                 for j in episodes[i]:
                     title = episodes[i][j]['title']
                     if j < 10:
-                        ep_num = "E0" + str(j)
+                        epNum = "E0" + str(j)
                     else:
-                        ep_num = "E" + str(j)
-                    f.write("S0" + str(i) + ep_num + " : " + title + "\n")
+                        epNum = "E" + str(j)
+                    f.write("S0" + str(i) + epNum + " : " + title + "\n")
 
     def createEpList(self, done):
         loadingProcess = threading.Thread(target = asciiArt.loadingRod)
@@ -94,55 +94,74 @@ class episodeList():
         # from randomised line
         file = open("episode_list.txt")
         episodes = file.readlines()
-        rand_episode = random.randint(0, 187)
-        sel_ep = episodes[rand_episode]
-        print(episodes[rand_episode])
-        return sel_ep
+        randEpisode = random.randint(0, 187)
+        selEp = episodes[randEpisode]
+        print(episodes[randEpisode])
+        return selEp
 
-    def modifyEpString(self, s_str, ep_str):
+    def modifyEpString(self, sStr, epStr):
         
         # Change episode string if there is two-part
         # episode available
-        if ((s_str == "S03") and (ep_str == "E12" or ep_str == "E13")):
-            ep_str = "E12E13"
-        elif (s_str == "S04"):
-            if (ep_str == "E01" or ep_str == "E02"):
-                ep_str = "E01E02"
-            elif (ep_str == "E03" or ep_str == "E04"):
-                ep_str = "E03E04"
-            elif (ep_str == "E05" or ep_str == "E06"):
-                ep_str = "E05E06"
-            elif (ep_str == "E07" or ep_str == "E08"):
-                ep_str = "E07E08"
-            elif (ep_str == "E18" or ep_str == "E19"):
-                ep_str = "E18E19"
-        elif (s_str == "S06"):
-            if (ep_str == "E04" or ep_str == "E05"):
-                ep_str = "E04E05"
-            elif (ep_str == "E13"):
+        if ((sStr == "S03") and (epStr == "E12" or epStr == "E13")):
+            epStr = "E12E13"
+        elif (sStr == "S04"):
+            if (epStr == "E01" or epStr == "E02"):
+                epStr = "E01E02"
+            elif (epStr == "E03" or epStr == "E04"):
+                epStr = "E03E04"
+            elif (epStr == "E05" or epStr == "E06"):
+                epStr = "E05E06"
+            elif (epStr == "E07" or epStr == "E08"):
+                epStr = "E07E08"
+            elif (epStr == "E18" or epStr == "E19"):
+                epStr = "E18E19"
+        elif (sStr == "S06"):
+            if (epStr == "E04" or epStr == "E05"):
+                epStr = "E04E05"
+            elif (epStr == "E13"):
 
                 # Prompt the user if extended version
                 # of episode should be played
-                input_str = input("Do you want to open an extended version of S06E13? (Y/N): ")
-                if input_str == 'Y' or input_str == 'y':
-                    ep_str = ep_str + " - EXTENDED"
+                inputStr = input("Do you want to open an extended version of S06E13? (Y/N): ")
+                if inputStr == 'Y' or inputStr == 'y':
+                    epStr = epStr + " - EXTENDED"
                 else:
                     pass
-            elif (ep_str == "E17" or ep_str == "E18"):
-                ep_str = "E17E18"        
-        elif (s_str == "S07"):
-            if (ep_str == "E11" or ep_str == "E12"):
-                ep_str = "E11E12"
-            elif (ep_str == "E25" or ep_str == "E26"):
-                ep_str = "E25E26"
+            elif (epStr == "E17" or epStr == "E18"):
+                epStr = "E17E18"        
+        elif (sStr == "S07"):
+            if (epStr == "E11" or epStr == "E12"):
+                epStr = "E11E12"
+            elif (epStr == "E25" or epStr == "E26"):
+                epStr = "E25E26"
         
-        return s_str, ep_str
-        
+        return sStr, epStr
+
+class pathClass():
+    def gotoParentDir(self):
+        # Changing directory to parent directory
+        currPath = os.path.dirname(os.getcwd())
+        os.chdir(currPath)
+        parPath = os.getcwd()
+
+        return parPath
+
+    def openEpisode(self):
+        # Open video in specified path
+        pathToOpen = parPath + "\\" + sStr + "\\" + sStr + epStr + ".mp4"
+        os.startfile(pathToOpen)
+
+    def revertDir(self):
+        # Revert path to old directory
+        os.chdir(parPath + "\\TheOfficeEpisodeGenerator\\")
+
 
 if __name__ == "__main__":
 
     asciiArt = asciiArt()
     episodeList = episodeList()
+    pathClass = pathClass()
 
     while (True):
 
@@ -153,8 +172,8 @@ if __name__ == "__main__":
         asciiArt.getASCII()
 
         # Check if file exists
-        file_path = pathlib.Path("episode_list.txt")
-        if file_path.is_file():
+        filePath = pathlib.Path("episode_list.txt")
+        if filePath.is_file():
             pass
         else:
             # Creating Episode List
@@ -163,29 +182,21 @@ if __name__ == "__main__":
 
         # Open episode list and obtain episode name
         # from randomised line
-        sel_ep = episodeList.loadEpList()
+        selEp = episodeList.loadEpList()
 
         # Parse string and episode number from line
-        s_str = sel_ep[:3]
-        ep_str = sel_ep[3:6]
+        sStr = selEp[:3]
+        epStr = selEp[3:6]
 
-        # Changing directory to parent directory
-        curr_path = os.path.dirname(os.getcwd())
-        os.chdir(curr_path)
-        par_path = os.getcwd()
-
-        s_str, ep_str = episodeList.modifyEpString(s_str, ep_str)
-
-        # Open video in specified path
-        path_to_open = par_path + "\\" + s_str + "\\" + s_str + ep_str + ".mp4"
-        os.startfile(path_to_open)
+        parPath = pathClass.gotoParentDir()
+        sStr, epStr = episodeList.modifyEpString(sStr, epStr)
+        pathClass.openEpisode()
 
         print("-> Enter any key to restart\n-> Enter 'Q' to Quit\n")
-        input_str = input("Enter an input: ")
-        if input_str == 'Q' or input_str == 'q':
+        inputStr = input("Enter an input: ")
+        if inputStr == 'Q' or inputStr == 'q':
             quit()
         else:
             pass
 
-        # Revert path to old directory
-        os.chdir(par_path + "\\TheOfficeEpisodeGenerator\\")
+        pathClass.revertDir()
